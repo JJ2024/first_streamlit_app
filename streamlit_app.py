2928b304dc817d307ef87d2df52eca8637120da8
John Jireh B. Garcia <67573779+JJ2024@users.noreply.github.com>
2022-06-28 16:35:40 +0800

    Update streamlit_app.py
    remade fruityvice_response into .json formatting
    added header for fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,5 +21,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
+st.header('Fruityvice Fruit Advice!')
 fruityvice_response = rq.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
