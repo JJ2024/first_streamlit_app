b70ef68e67e8b5ecf4dbd6f8bc65a62ed3db3d60
John Jireh B. Garcia <67573779+JJ2024@users.noreply.github.com>
2022-06-28 16:53:02 +0800

    Update streamlit_app.py
    added custom input to get fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,10 +22,13 @@ st.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
-fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered', fruit_choice)
+fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #take the json version of the response and normalize it
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
 #output it to the screen as a table
 st.dataframe(fruityvice_normalized)
+
