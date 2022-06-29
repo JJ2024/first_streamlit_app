4f6b8394cca423344e64c12825aedf29e44ab0ae
John Jireh B. Garcia <67573779+JJ2024@users.noreply.github.com>
2022-06-29 21:20:57 +0800

    Update streamlit_app.py
    changed "###" positions

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 st.dataframe(fruits_to_show)
-###
+
 #New section to display fruityvice api response
-###
+
 fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
 st.write('The user entered', fruit_choice)
 fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
