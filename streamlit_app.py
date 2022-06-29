9cf91993927cddb1c3aa9bee3c18e3f70ab9c5b5
John Jireh B. Garcia <67573779+JJ2024@users.noreply.github.com>
2022-06-29 22:35:06 +0800

    Update streamlit_app.py
    #Snowflake-related functions
    def get_fruit_load_list():
        with my_cnx.cursor() as my_cur:
            my_cur.execute("select * from fruit_load_list")
            return my_cur.fetchall()
                            
    #Add a button to load the fruit
    if st.button('Get Fruit Load List'):
        my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
        my_data_rows = get_fruit_load_list()
        st.dataframe(my_data_rows)

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,8 @@ fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
-#create the repeatable code block (called a function)
+
+#create the repeatable code block for getting fruityvice data (called a function)
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
@@ -31,7 +32,6 @@ try:
   fruit_choice = st.text_input('What fruit would you like information about?')
   if not fruit_choice:
     st.error("Please select a fruit to get information.")
-    
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     st.dataframe(back_from_function)
@@ -39,14 +39,18 @@ try:
 except URLError as e:
   st.error()
 
-st.stop()
-  
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
-st.header("The fruit list contains:")
-st.dataframe(my_data_row)
+
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+                        
+#Add a button to load the fruit
+if st.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    st.dataframe(my_data_rows)
 
 st.header('What fruit would you like to add?')
 add_my_fruit = st.text_input('Input fruit')
