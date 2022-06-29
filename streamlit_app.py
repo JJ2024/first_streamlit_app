4420705bea4d78824160289a81bdeba9de62aa99
John Jireh B. Garcia <67573779+JJ2024@users.noreply.github.com>
2022-06-29 21:35:31 +0800

    Update streamlit_app.py
    try:  
      fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
      if not fruit_choice:
        st.error("Please select a fruit to get information.")
        
      else:
        fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
        st.dataframe(fruityvice_normalized)
        
    except URLError as e:
      st.error()

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,24 +19,28 @@ st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 st.dataframe(fruits_to_show)
 
-#New section to display fruityvice api response
+#New section for Fruityvice API Response
+st.header ('Fruityvice Fruit Advice!')
 
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered', fruit_choice)
-fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+try:  
+  fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choice:
+    st.error("Please select a fruit to get information.")
+    
+  else:
+    fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  st.error()
 
-#take the json version of the response and normalize it
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-
-#output it to the screen as a table
-st.dataframe(fruityvice_normalized)
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
