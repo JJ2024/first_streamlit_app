d5257a5fe0aec54e442e1d77f76a93215c6431ae
John Jireh B. Garcia <67573779+JJ2024@users.noreply.github.com>
2022-06-29 20:55:41 +0800

    Update streamlit_app.py
    y = my_cur.execute(f"insert into fruit_load_list values('{add_my_fruit}')")

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,4 @@ st.dataframe(my_data_row)
 
 st.header('What fruit would you like to add?')
 add_my_fruit = st.text_input('Input fruit')
-x = my_cur.execute(f"select * from fruit_load_list where fruit_name = '{add_my_fruit}'")
-st.dataframe(x)
+y = my_cur.execute(f"insert into fruit_load_list values('{add_my_fruit}')")
