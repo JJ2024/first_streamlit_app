55ec8400d347e7b46089f5021fc5ed7092a61420
John Jireh B. Garcia <67573779+JJ2024@users.noreply.github.com>
2022-06-29 22:15:02 +0800

    Update streamlit_app.py
    #create the repeatable code block (called a function)
    def get_fruityvice_data(this_fruit_choice):
        fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
        return fruityvice_normalized
     
    #New section for Fruityvice API Response
    st.header('Fruityvice Fruit Advice!')
    try:  
      fruit_choice = st.text_input('What fruit would you like information about?')
      if not fruit_choice:
        st.error("Please select a fruit to get information.")
        
      else:
        back_from_function = get_fruityvice_data(fruit_choice)
        st.dataframe(back_from_function)
        
    except URLError as e:
      st.error()

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import snowflake.connector
 from urllib.error import URLError
 
 
-
 st.title('My Parents New Healthy Diner')
 st.text("🥣 Omega 3 & Blueberry Oatmeal")
 st.text("🥗 Kale, Spinach & Rocket Smoothie")
@@ -13,27 +12,29 @@ st.text("🐔 Hard-Boiled Free-Range Egg")
 st.text("🥑🍞 Hard-Boiled Free-Range Egg")
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+#For fruityvice selection only
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
 st.dataframe(fruits_to_show)
 
+#create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+ 
 #New section for Fruityvice API Response
-st.header ('Fruityvice Fruit Advice!')
-
+st.header('Fruityvice Fruit Advice!')
 try:  
-  fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = st.text_input('What fruit would you like information about?')
   if not fruit_choice:
     st.error("Please select a fruit to get information.")
     
   else:
-    fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-    st.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    st.dataframe(back_from_function)
     
 except URLError as e:
   st.error()
