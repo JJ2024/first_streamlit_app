ebe4e95f07783f1f2fb8adfcfd09cbc33cd53663
John Jireh B. Garcia <67573779+JJ2024@users.noreply.github.com>
2022-11-08 23:45:49 +0800

    Update streamlit_app.py
    Added:
    
    "my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_cur = my_cnx.cursor()
    my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
    my_data_row = my_cur.fetchone()
    streamlit.text("Hello from Snowflake:")
    streamlit.text(my_data_row)
    "

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ if st.button('Add Value'):
     my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
     insert_row_snowflake(add_fruit)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake2"])
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
